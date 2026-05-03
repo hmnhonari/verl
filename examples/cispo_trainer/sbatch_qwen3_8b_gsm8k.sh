51ed540c46fb40cae22cda0b07d465096f1cd430
Homayoun Honari <hmnhonari@gmail.com>
2026-05-03 12:28:42 -0400

    revert to 8 gpu node

--- examples/cispo_trainer/sbatch_qwen3_8b_gsm8k.sh
+++ examples/cispo_trainer/sbatch_qwen3_8b_gsm8k.sh
@@ -2,12 +2,12 @@
 
 #SBATCH --account=aip-gberseth
 #SBATCH --ntasks=1
-#SBATCH --gpus-per-node=h100:4
+#SBATCH --gpus-per-node=h200:8
 #SBATCH -o /scratch/h/homayoon/slurm-%j.out
 #SBATCH --time=1-00:00:00
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 #SBATCH --mem=0
-#SBATCH --cpus-per-task=48
+#SBATCH --cpus-per-task=64
 
 module load apptainer/1.4.5
 module load httpproxy
@@ -26,9 +26,9 @@ apptainer_image_path=/scratch/h/homayoon/verl/verl.sif
 # --- Start Ray Head Node ---
 # On a single node, we just start the head. No need for IP detection or worker loops.
 echo "Starting Ray head on $(hostname)"
-srun --nodes=2 --ntasks=1 \
+srun --nodes=1 --ntasks=1 \
    apptainer run --nv --bind $verl_workdir $apptainer_image_path \
-   ray start --head --port=6379 --num-cpus "${SLURM_CPUS_PER_TASK}" --num-gpus=4 --block &
+   ray start --head --port=6379 --num-cpus "${SLURM_CPUS_PER_TASK}" --num-gpus=8 --block &
 
 # Wait a moment for Ray to initialize
 sleep 10
